ba64d4db6ffe3db938b8cdb988e297d4fb0ba3a1
thuan.doan <doangia.sw@gmail.com>
2022-08-04 22:36:37 +0700

    add unit test for repository and service

--- TeduShop.Data/Repositories/VisitorStatisticRepository.cs
+++ TeduShop.Data/Repositories/VisitorStatisticRepository.cs
@@ -5,18 +5,12 @@ namespace TeduShop.Data.Repositories
 {
     public interface IVisitorStatisticRepository : IRepository<VisitorStatistic>
     {
-        int GetOnline(string alias);
     }
 
     public class VisitorStatisticRepository : RepositoryBase<VisitorStatistic>, IVisitorStatisticRepository
     {
         public VisitorStatisticRepository(IDbFactory dbFactory) : base(dbFactory)
         {
-        }
-
-        public int GetOnline(string alias)
-        {
-            throw new System.NotImplementedException();
         }
     }
 }
---
+++ TeduShop.UnitTest/RepositoryTest/PostCategoryRepositoryTest.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeduShop.Data.Infrastructure;
+using TeduShop.Data.Repositories;
+using TeduShop.Model.Models;
+
+namespace TeduShop.UnitTest.RepositoryTest
+{
+    [TestClass]
+    public class PostCategoryRepositoryTest
+    {
+        IDbFactory dbFactory;
+        IPostCategoryRepository objRepositoty;
+        IUnitOfWork unitOfWork;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            dbFactory = new DbFactory();
+            objRepositoty = new PostCategoryRepository(dbFactory);
+            unitOfWork = new UnitOfWork(dbFactory);
+        }
+
+        [TestMethod]
+        public void PostCategory_Repository_GetAll()
+        {
+            var list = objRepositoty.GetAll();
+            Assert.IsNotNull(list);
+        }
+
+
+        [TestMethod]
+        public void PostCategory_Repository_Create()
+        {
+            PostCategory category = new PostCategory();
+            category.Name = "Test category";
+            category.Alias = "test-category";
+            category.Status = true;
+            var result = objRepositoty.Add(category);
+            unitOfWork.Commit();
+            Assert.IsNotNull(result);
+            //Assert.AreEqual(1, result.ID);
+            
+        }
+    }
+}
